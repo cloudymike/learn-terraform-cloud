c6ee985cc62f8fd23da08c283b3459e7d4d1ed21
cloudymike <mikael@cloudyforecast.com>
2022-08-24 22:05:27 -0700

    Remove cloud block

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "us-west-2"
 }
 
 variable "instance_type" {
@@ -12,4 +12,3 @@ variable "instance_name" {
   description = "EC2 instance name"
   default     = "Provisioned by Terraform"
 }
-
